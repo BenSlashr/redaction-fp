bd9623da09d85a5a36d3fca8aacef7852b60b022
BenSlashr <benoit@slashr.fr>
2025-04-09 16:27:55 +0200

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configuration de l'URL de l'API
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8050';
 
 // Types pour les requêtes et réponses
 interface ProductInfo {
